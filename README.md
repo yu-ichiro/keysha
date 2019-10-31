88a14d2c1d7569a2ccc1bf1f0455d2439fa5467c
Yuichiro Luke Smith <contact@yu-smith.com>
2019-10-31 23:25:40 +0900

    Hello World

---
+++ README.md
@@ -0,0 +1,23 @@
+# Keysha
+
+> An application that helps going dutch with tiers
+
+## Usage
+
+``` bash
+# Install dependencies
+npm install
+
+# Preview on device
+tns preview
+
+# Build, watch for changes and run the application
+tns run
+
+# Build, watch for changes and debug the application
+tns debug <platform>
+
+# Build for production
+tns build <platform> --env.production
+
+```
---
+++ app/components/App.vue
@@ -0,0 +1,32 @@
+<template>
+    <Page>
+        <ActionBar title="Welcome to NativeScript-Vue!"/>
+        <GridLayout columns="*" rows="*">
+            <Label class="main-text" :text="msg" />
+        </GridLayout>
+    </Page>
+</template>
+
+<script lang="ts">
+  import {Component} from 'vue-property-decorator';
+  import NativeScriptVue from 'nativescript-vue';
+
+  @Component
+  export default class App extends NativeScriptVue {
+    msg = 'Hello World!';
+  }
+</script>
+
+<style scoped>
+    ActionBar {
+        background-color: #53ba82;
+        color: #ffffff;
+    }
+
+    .main-text {
+        color: #4e4e4e;
+        vertical-align: center;
+        text-align: center;
+        font-size: 20;
+    }
+</style>
